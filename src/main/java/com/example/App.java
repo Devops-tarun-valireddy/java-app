3fb2ab696a754ddfe044fbaa173310886dfeb77d
Tarunvalireddy63 <145964752+tarunvalireddy@users.noreply.github.com>
2025-06-25 11:42:20 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,10 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from Jenkins!");
+        while (true) {
+            Thread.sleep(10000); // Sleep for 10 seconds in an infinite loop
+        }
     }
 }
-
